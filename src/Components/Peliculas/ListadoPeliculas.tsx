735e2535df60ebeb00d28e6569a008eabfbdd08e
Oliver Nunez <olivern.cristo@gmail.com>
2021-08-10 04:12:50 +0800

    Un comit nuevo

---
+++ src/Components/Peliculas/ListadoPeliculas.tsx
@@ -0,0 +1,24 @@
+import PeliculaIndividual from "./PeliculaIndividual";
+import { pelicula } from "./peliculas.model";
+import css from './ListadoPeliculas.module.css';
+import Cargando from "../Utils/Cargando";
+import ListadoGenerico from "../Utils/ListadoGenerico";
+
+export default function ListadoPeliculas(props:listadoPeliculasProps){
+
+        return(
+            <>
+                <ListadoGenerico listado = {props.peliculas}>
+                    <div className={css.div}>
+                        {props.peliculas?.map(peli =>
+                            <PeliculaIndividual key={peli.id} pelicula={peli} />
+                        )}
+                    </div>
+                </ListadoGenerico>
+            </>
+        )
+}
+
+interface listadoPeliculasProps{
+    peliculas?: pelicula[]
+}
---
+++ src/Components/Utils/Cargando.tsx
@@ -0,0 +1,12 @@
+import css from './Cargando.module.css';
+
+export default function Cargando(){
+
+    return(
+        <>
+            <div >
+                <img className={css.div} src="https://icon-library.com/images/loading-icon-animated-gif/loading-icon-animated-gif-19.jpg" alt="Cargando" />
+            </div>
+        </>
+    )
+}
